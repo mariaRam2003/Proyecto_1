fd4a3abdfc53562ab8bbf66612d8a5390a2b3f52
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-09 00:13:06 -0600

    First Section
    Poner mas bonito el primer section de la pagina para que se asemej mas a la pagina original. Agregar animaciones, centrar boton, cambio de color. :goa

--- tesla-copy/public/scroll.js
+++ tesla-copy/public/scroll.js
@@ -28,5 +28,18 @@ document.addEventListener('scroll', () => {
         }
     })
 
+    // Aplicar la clase "first-section" al primer section, header y logo
+    const header = document.querySelector('header');
+    const logo = document.querySelector('a.logo');
+    if (document.lastCentered === 0) {
+        sections[0].classList.add('first-section');
+        header.classList.add('first-section');
+        logo.classList.add('first-section');
+    } else {
+        sections[0].classList.remove('first-section');
+        header.classList.remove('first-section');
+        logo.classList.remove('first-section');
+    }
+
     document.lastScrollPosition = window.pageYOffset;
 })
